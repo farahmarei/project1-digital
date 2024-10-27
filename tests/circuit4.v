bee6e4e0a091bb264eab4a4c83511d01ba218c42
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-10-27 22:35:47 +0300

    Update circuit4.v

--- tests/circuit4.v
+++ tests/circuit4.v
@@ -1,6 +1,19 @@
+module circuit_4(a, b, c, o);
+    input a;
+    input b;
+    input c;
+        
+    output o;
+        
+    wire w;
+    wire y;
+    wire n;
+    wire z;
+        
+    and  #(4) g0 (w, a, b);
+    or   #(6) g1 (y, w, c);
+    nor  #(5) g2 (n, a, y);
+    nand #(3) g3 (z, b, n);
+    xor  #(7) g4 (o, z, c);
 
-module circuit4(input a,input b, input c, output f);
-    assign f = ~(a | c) | (a |~b);
-endmodule
-
-
+endmodule;
