7018f01f5cb26ccbb04ea89d9d2e4a0d2679abb7
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-10-27 22:35:32 +0300

    Update circuit3.v

--- tests/circuit3.v
+++ tests/circuit3.v
@@ -1,5 +1,19 @@
+module circuit_3(a, b, c, o);
+    input a;
+    input b;
+    input c;
+        
+    output o;
+        
+    wire w;
+    wire y;
+    wire n;
+    wire z;
+        
+    nand #(5) g0 (w, a, b);
+    xor  #(3) g1 (y, w, c);
+    not  #(2) g2 (n, a);
+    or   #(7) g3 (z, n, y);
+    nor  #(4) g4 (o, z, c);
 
-module circuit3(input a,input b, input c, output f);
-    assign f = (a&~b) ^ (~c | a);
-endmodule
-
+endmodule;
