fffb5272c26fc2d54d94114505904d6c34e5a411
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-10-27 22:34:54 +0300

    Update circuit1.v

--- tests/circuit1.v
+++ tests/circuit1.v
@@ -1,6 +1,20 @@
+module circuit_1(a, b, c, o);
+    input a;
+    input b;
+    input c;
+        
+    output o;
+        
+    wire w;
+    wire y;
+    wire n;
+    wire z;
+        
+    nand #(4) g0 (w, a, b);
+    xor  #(2) g1 (y, w, c);
+    and  #(7) g2 (n, a, y);
+    nor  #(6) g3 (z, b, n);
+    or   #(3) g4 (o, z, c);
 
-module circuit1(input a,input b, input c, output f);
-    assign f = (~a&c) & (b | a);
-endmodule
-
+endmodule;
 
