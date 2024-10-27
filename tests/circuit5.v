4591820a8f65428e8ea34e26866159e105482be4
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-10-27 22:36:09 +0300

    Update circuit5.v

--- tests/circuit5.v
+++ tests/circuit5.v
@@ -1,3 +1,19 @@
-module circuit5(input a,input b, input c, output f);
-    assign f = (~(a&b)|c) | ((b&~c) | a);
-endmodule
+module circuit_5(a, b, c, o);
+    input a;
+    input b;
+    input c;
+        
+    output o;
+        
+    wire w;
+    wire y;
+    wire n;
+    wire z;
+        
+    or   #(6) g0 (w, a, b);
+    nand #(3) g1 (y, w, c);
+    xor  #(5) g2 (n, a, y);
+    and  #(4) g3 (z, b, n);
+    nor  #(7) g4 (o, z, c);
+
+endmodule;
