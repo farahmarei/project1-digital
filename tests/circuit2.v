2c982da05a16611c93d9bd639d19c021f74edcb6
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-10-27 22:35:12 +0300

    Update circuit2.v

--- tests/circuit2.v
+++ tests/circuit2.v
@@ -1,4 +1,19 @@
-module circuit2(input a,input b, input c, output f);
-    assign f = ~(a&b) ^ ((b&c) | a);
-endmodule
+module circuit_2(a, b, c, o);
+    input a;
+    input b;
+    input c;
+        
+    output o;
+        
+    wire w;
+    wire y;
+    wire n;
+    wire z;
+        
+    nor #(3) g0 (w, a, b);
+    and #(6) g1 (y, w, z);
+    xor #(4) g2 (n, a, y);
+    or  #(8) g3 (z, b, c);
+    and #(2) g4 (o, n, z);
 
+endmodule;
