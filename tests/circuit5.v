d6bd7745501699abb5e62a02a52d942a5761c72d
hannashehab <124671977+hannashehab@users.noreply.github.com>
2024-11-02 13:44:39 +0200

    Update circuit5.v

--- tests/circuit5.v
+++ tests/circuit5.v
@@ -11,7 +11,8 @@ module circuit_5(a, b, c, o);
     wire y; //Intermediate wire for NAND gate output
     wire n; //Intermediate wire for XOR gate output
     wire z; //Intermediate wire for AND gate output
-        
+
+    //Instantiate gates with specific delays
     or   #(6) g0 (w, a, b); //OR gate with delay of 6, output connected to w, inputs a and b
     nand #(3) g1 (y, w, c); //NAND gate with delay of 3, output connected to y, inputs w and c
     xor  #(5) g2 (n, a, y); //XOR gate with delay of 5, output connected to n, inputs a and y
